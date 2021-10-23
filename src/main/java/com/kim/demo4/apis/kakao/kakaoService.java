bae261c4a4cb065ff402ee64141b520024e60188
novb1492 <novb1492@naver.com>
2021-10-24 01:06:35 +0900

    ++

--- src/main/java/com/kim/demo4/apis/kakao/kakaoService.java
+++ src/main/java/com/kim/demo4/apis/kakao/kakaoService.java
@@ -45,15 +45,16 @@ public class kakaoService {
 	        String scope=request.getParameter("scope");
 	        if(scope.equals("login")){
 	            LOGGER.info("카카오 로그인 콜백");
-	            tryKakaoLogin(request);
+	            tryKakaoLogin(request,response);
 	        }else{
 	            throw new RuntimeException("카카오 콜백 잘못된 스코프입니다");
 	        }
 	    }
-	    private void tryKakaoLogin(HttpServletRequest request) {
+	    private void tryKakaoLogin(HttpServletRequest request,HttpServletResponse response) {
 	        LOGGER.info("tryKakaoLogin");
 	        memberDto uservo =kakaoLoginService.tryLogin(request,apiKey);
-	       
+	        request.getSession().setAttribute("email", uservo.getEmail());
+	       utillService.doRedirect(response, "/demo4/doneLoingPage");
 	       
 	    }
 }
